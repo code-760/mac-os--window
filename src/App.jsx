e81a6d83ff8e85ae35b3ffb334b96ac1c33245e0
Himanshu <himanshu760kumawat@gmail.com>
2026-06-07 11:56:17 +0530

    Remove unused imports in App.jsx
    Removed unused page imports from App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,7 @@ import "./App.scss";
 import Fotter from "./componnts/Fotter";
 import Nav from "./componnts/Nav";
 import Manu from "./componnts/Manu";
-import Window from "./pages/window";
-import Github from "./pages/github";
-import Email from "./pages/Email";
-import Linkdine from "./pages/linkdine";
-import Pdf from "./pages/pdf";
-import Terminle from "./pages/terminle";
-import Notes from "./pages/Notes";
-import Cal from "./pages/cal";
+
 
 
 export default function App() {
